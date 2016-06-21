a3d7337217809a5b7c75a60574e45472f5c2d259
Gus <you@example.com>
2016-06-20 21:20:36 -0300

    CSV com ,

--- html/lista_cnae.php
+++ html/lista_cnae.php
@@ -1,3 +1,4 @@
+<?php require_once '../processa/bd.class.php'?>
 <html>
 	<head>
 		<link rel="stylesheet" href="style.css">
@@ -7,23 +8,25 @@
 	<body>
 		<p class="centraliza">
 		<a href="novo_tst.php" style='text-decoration: none;'>Novo</a>
-		<table style="width:50%">
+	<table style="width:50%">
 		  <tr>
 		    <!--Nomes Campos-->
-			<th>Código</th> <th>Deniminação</th> <th>GR</th>
+			<th> Cód CNAE</th> <th>Risco</th> <th>Descrição</th>
 		  </tr>
-		  <tr>
-		    <td>01</td> <td>AGRICULTURA, PECUÁRIA E SERVIÇOS RELACIONADOS</td> <td>-</td>
-			<td class="td"><a href="edita_tst.php">Edita</a> <a href="deleta_tst.php">Deleta</a></td>
-		  </tr>
-		  <tr>
-		    <td>01.1</td> <td>Produção de lavouras temporárias</td> <td>-</td>
-			<td class="td"><a href="edita_tst.php">Edita</a> <a href="deleta_tst.php">Deleta</a></td>
-		  </tr>
-		  <tr>
-		    <td>01.11-3</td> <td>Cultivo de cereais</td> <td>3</td>
-			<td class="td"><a href="edita_tst.php">Edita</a> <a href="deleta_tst.php">Deleta</a></td>
-		  </tr>
+		<?php
+		$resultado = $bd->get_all('cnae');
+		while($linha = mysqli_fetch_array($resultado)){
+			
+			$where = "WHERE id = '".$linha['id_risco']."'";
+			$resultado_risco = $bd->get_all('risco', $where);
+			while($linha_risco = mysqli_fetch_array($resultado_risco)){
+			echo '<tr>';
+			echo "<td>".$linha['cnae']."</td> <td>".$linha_risco['risco'].'</td> <td>'.$linha['descricao'].'</td>';
+			echo '<td class="td"><a href="edita_tst.php">Edita</a> <a href="deleta_tst.php">Deleta</a></td>';
+			echo '</tr>';
+			}
+		}
+		?>
 		</table>
 	</body>
 </html>
