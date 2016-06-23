1f8180715adc355f948c4d366004fbd878d030b0
Code <code@senacrs.edu.br>
2016-06-22 21:52:07 -0300

    listas

--- html/lista_emp_tst.php
+++ html/lista_emp_tst.php
@@ -1,5 +1,5 @@
 <?php  require_once '../processa/bd.class.php' ?>
-<html>da
+<html>
 	<head>
 		<link rel="stylesheet" href="style.css">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -14,20 +14,31 @@
 	<body>
 		<?php include "menu.php"; ?>	
 		<p class="centraliza">
-		<a href="novo_tst.php" style='text-decoration: none;'>Novo</a>
-	<table style="width:50%">
-		  <tr>
+		<table>
+			<thead class="topo_lista">
+				<tr>
+					<td colspan="4">
+						<strong>Lista de Grupos<strong>
+					</td>
+				</tr>
+			</thead>
+	
+		   <tr class="topo_tabela">
 		    <!--Nomes Campos-->
 
 
 
-		    <th>Empregado</th><th>Descrição</th>
+		    <th> <style='width: 50%'>Empregado</th>
+			<th><style='width: 50%'>Descrição</th>
+			<th colspan="2">
+			<a href="novo_tst.php" style='text-decoration: none;'> <style='width: 50%'>Novo</a>
+			</th>
 		  </tr>
 			<?php
 			$resultado = $bd->get_all('empregado');
 			while($linha = mysqli_fetch_array($resultado)){
 					echo "<tr>
-							<td>".
+							<td>". 
 								$linha['empregado']
 							."</td>
 							<td>".
