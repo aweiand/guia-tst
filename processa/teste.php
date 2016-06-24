25e0ffc2f2ed12e6188c7f51833226a4f8b8ab02
gabyte <gabiflores9515@gmail.com>
2016-06-23 21:43:50 -0300

    func

---
+++ processa/teste.php
@@ -0,0 +1,16 @@
+teste.php
+
+<html>
+	<head>
+
+	</head>
+	<body>
+		<form method="GET" action="metodos_cnae.cipa.php">
+		CNAE
+		<br />
+		<input type="text" name="cipa"/>
+		<br />
+		<button> enviar </button>
+		</form>
+	</body>
+</head>
