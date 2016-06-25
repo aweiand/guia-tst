c5a5158610d4bafdfbc8661e0858b357c2b71f60
professor <professor@senacrs.edu.br>
2016-06-24 21:59:30 -0300

    iniciando os ajustes na 2a tela

--- html/result_tst.php
+++ html/result_tst.php
@@ -1,58 +1,129 @@
-<!DOCTYPE html>
 <html>
   <head>
-  <style>
-		.central{
-			position: absolute;
-			text-align: center;
-			left: 25%;
-			top: 23%;
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="font-awesome-4.6.2/css/font-awesome.min.css">
+	<style>
+		.tbl-consulta {
 			width: 50%;
-			font-size: 20px;
-			
+			border: 1px solid black;
+			margin: 0 auto;
 		}
-		</style>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
-    <title>Pesquisa</title>
+	</style>
+	<title>Resultado da Pesquisa</title>
   </head>
   <body>
-    <div class="cabecalho">
-        CNAE
-		<ul>
-          <li><a>CNAE:</a><a>15</a></li><br>
-          <li><a>Descrição:</a><a>PREPARAÇÃO DE COUROS E FABRICAÇÃO DE ARTEFATOS DE COURO, ARTIGOS PARA VIAGEM E CALÇADOS</a></li><br>
-          <li><a>Risco:</a><a>1</a></li><br>
-       
-      
-		
-      <li>Técnico Seg. Trabalho - </li><br>
-
-      <li>Engenheiro Seg. Trabalho - </li><br>
-
-      <li>Aux. Enfermagem Trabalho - </li><br>
-
-      <li>Enfermeiro do Trabalho - </li><br>
-
-      <li>Médico do Trabalho - </li><br>
-		</ul>
-	  </div>
-	  <div class="cabecalho">
-        CIPA
-		<ul>
-          <li><a>GRUPO CNAE:</a><a>15</a></li><br>
-          <li><a>Descrição:</a><a>PREPARAÇÃO DE COUROS E FABRICAÇÃO DE ARTEFATOS DE COURO, ARTIGOS PARA VIAGEM E CALÇADOS</a></li><br>
-          <li><a>Suplentes:</a><a>1</a></li><br>
-		  <li><a>Efetivos:</a><a>0</a></li><br>
-       
-      
-		  </div>
-	  
-	   
-	   <footer class="footer">
-      <br><a href="pesquisa_tst.php">Voltar</a>
-    </footer>
+	<?php require_once '../processa/bd.class.php' ?>
+	<div>
+		<img class="logo" src="img/logo1.png" alt="logo" style="width: 10%; float: right; margin-right: 10%; margin-top: 1%;"/>
+	</div>
+	
+    <table class="tbl-consulta">
+		<thead>
+			<tr>
+				<td colspan="3">
+					CNAE 000-000-00
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					Descrição
+				</td>
+				<td colspan="2">
+					Risco
+				</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td colspan="3">
+					Descrição Descrição Descrição Descrição
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3">
+					Intervalo de funcionários 0 à 50
+				</td>
+			</tr>
+			<tr>
+				<td>
+					-->
+				</td>
+				<td>
+					Técnico em Segurança do Trabalho
+				</td>
+				<td>
+					4
+				</td>
+			</tr>
+			<tr>
+				<td>
+					-->
+				</td>
+				<td>
+					Técnico em Segurança do Trabalho
+				</td>
+				<td>
+					1
+				</td>
+			</tr>
+			<tr>
+				<td>
+					-->
+				</td>
+				<td>
+					Técnico em Segurança do Trabalho
+				</td>
+				<td>
+					0
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	
+	<hr />
+	
+	<table class="tbl-consulta">
+		<thead>
+			<tr>
+				<td colspan="3">
+					CIPA
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3">
+					Grupo CNAE C1-a
+				</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>
+					-->
+				</td>
+				<td>
+					Efetivo
+				</td>
+				<td>
+					1
+				</td>
+			</tr>
+			<tr>
+				<td>
+					-->
+				</td>
+				<td>
+					Suplente
+				</td>
+				<td>
+					1
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	
+    <a href="pesquisa_tst.php">
+		<i  class="fa fa-3x fa-check-square-o fa-lg"></i>
+		Nova Consulta
+	</a>
   </body>
 </html>
-    </div>
-   
